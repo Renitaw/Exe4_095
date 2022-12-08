0d1fdeb5c2c2787aea62fec6a7748a39fd3c4bff
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-12-08 16:30:38 +0700

    Create a case for Push

--- Exe4/Program.cs
+++ Exe4/Program.cs
@@ -80,6 +80,15 @@ namespace Exe4_StackUsingArray
                 Console.WriteLine("\nEnter your choice: ");
                 string sInput = Console.ReadLine();
                 char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+
+                switch (ch)
+                {
+                    case '1':
+                        Console.WriteLine("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                }
             }
         }
     }
